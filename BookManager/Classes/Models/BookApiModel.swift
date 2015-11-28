1f7c0cdd4753a030c1b89b1ae1fe9045a63a2527
takayuki-hara <ffkfx888@yahoo.co.jp>
2015-11-28 12:05:28 +0900

    コメント追加

--- BookManager/Classes/Models/BookApiModel.swift
+++ BookManager/Classes/Models/BookApiModel.swift
@@ -8,11 +8,13 @@
 
 import Foundation
 
+/// 書籍検索結果用のAPI文字列を作成するクラス
 struct BookApiModel {
 	static let prot = "https://"
 	static let host = "app.rakuten.co.jp/"
 	static let api = "services/api/BooksBook/Search/20130522?"
 	
+	// MARK: - Static Methods
 	static func searchApiWithIsbn(isbn: String) -> String {
 		let urlString = prot + host + api + "format=json&carrier=1" + "&page=1" + "&title=\(isbn)" + "&applicationId=\(appId())"
 		return urlString.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
